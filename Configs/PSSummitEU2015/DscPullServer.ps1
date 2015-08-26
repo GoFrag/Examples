d5b875d14ecb303823c475d0d201b8f7364cbbc9
Mark Gray <mark@grayszone.com>
2015-08-26 14:59:18 -0700

    Updated configurations to resolve few bugs.

--- Configs/PSSummitEU2015/DscPullServer.ps1
+++ Configs/PSSummitEU2015/DscPullServer.ps1
@@ -1,31 +1,72 @@
-﻿
+﻿#region Generate certificate to be used to encrypt passwords in Pull Server config
+$Certificate = ls Cert:\LocalMachine\My | ? {$_.Subject -eq "CN=$env:COMPUTERNAME" -and $_.PrivateKey.KeyExchangeAlgorithm} | Select -First 1 
+if(!$Certificate)
+{
+    $Certificate = New-SelfSignedCertificate -Provider 'Microsoft RSA SChannel Cryptographic Provider' -DnsName $env:COMPUTERNAME -CertStoreLocation 'Cert:\LocalMachine\My'
+}
+
+#endregion Generate certificate to be used to encrypt passwords in Pull Server config
+
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName = "localhost"
+            CertificateID = $Certificate.Thumbprint
+            }
+    )
+}
+
+$SSLCertFilePath = 'C:\Git\Examples\Configs\PSSummitEU2015\my_ssl.pfx'
 
 Configuration V2PullServer
 {
+    param(
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()] 
+            [pscredential] $SSLCertificatePassword,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string] $SSLCertThumbprint
+    )
+
     Import-DscResource -ModuleName xPsDesiredStateConfiguration
-
-    WindowsFeature DSCServiceFeature
+    Import-DscResource -ModuleName xCertificate
+    node localhost
     {
-        Ensure = "Present"
-        Name   = "DSC-Service"            
-    }
+        WindowsFeature DSCServiceFeature
+        {
+            Ensure = "Present"
+            Name   = "DSC-Service"            
+        }
 
-    #Resource to install SSL Certificate
+        #Resource to install SSL Certificate
+        xPfxCertificate PullServerSSCert
+        {
+            Ensure = 'Present'
+            FilePath = $SSLCertFilePath
+            CertStoreLocation = 'Cert:\LocalMachine\My'
+            Password = $SSLCertificatePassword
+            Exportable = $false
+        }
 
-    xDscWebService PSDSCPullServer
-    {
-        Ensure                       = "Present"
-        EndpointName                 = "PSDSCPullServer"
-        Port                         = 443
-        PhysicalPath                 = "$env:SystemDrive\inetpub\wwwroot\PSDSCPullServer"
-        CertificateThumbPrint        = "3BD95654606A208CFF8B3316A0545F383592DBF5"         
-        ModulePath                   = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
-        ConfigurationPath            = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"            
-        State                        = "Started"
-        DependsOn                    = "[WindowsFeature]DSCServiceFeature" 
-        RegistrationKeyPath          = "$env:PROGRAMFILES\WindowsPowerShell\DscService"   
-        AcceptSelfSignedCertificates = $true
+        xDscWebService PSDSCPullServer
+        {
+            Ensure                       = "Present"
+            EndpointName                 = "PSDSCService"
+            Port                         = 443
+            PhysicalPath                 = "c:\inetpub\PullServer"
+            CertificateThumbPrint        = $SSLCertThumbprint                  
+            State                        = "Started"
+            DependsOn                    = "[WindowsFeature]DSCServiceFeature" 
+            AcceptSelfSignedCertificates = $true
+        }
     }
 }
 
-V2PullServer -OutputPath c:\Configs\
+$SSLcred = Get-Credential -Message 'Enter Password for pfx certificate: ' -UserName 'None'
+$SSLThumbprint = (Get-PfxCertificate -FilePath $SSLCertFilePath).Thumbprint
+
+V2PullServer -SSLCertificatePassword $SSLcred -SSLCertThumbprint $SSLThumbprint -ConfigurationData $ConfigData -OutputPath c:\Configs\MOF 
+
+#Start-DscConfiguration -Path C:\Configs\MOF\ -Wait -Verbose
