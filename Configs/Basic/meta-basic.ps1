f3bd210406062dd214909c93e1a0dd297cf4b07a
grayzu <mark@grayszone.com>
2016-01-19 10:16:39 -0800

    couple of basic configs

---
+++ Configs/Basic/meta-basic.ps1
@@ -0,0 +1,12 @@
+﻿[DscLocalConfigurationManager()]
+configuration metaconfig
+{
+    Settings
+    {
+        RefreshMode = "Push"
+    }
+}
+
+metaconfig -OutputPath C:\examples\MOF\meta
+
+Set-DscLocalConfigurationManager -Path C:\examples\MOF\meta -Verbose
---
+++ Configs/Basic/web.ps1
@@ -0,0 +1,67 @@
+﻿Configuration Web
+{
+    Import-DscResource -ModuleName xWebAdministration
+
+    Node localhost
+    {           
+        xWebsite Default
+        {
+            Ensure          = "Present"
+            Name            = "Default Web Site"
+            PhysicalPath    = "C:\inetpub\wwwroot"
+        }
+       
+        xWebsite ContosoSite
+        {
+            Ensure          = "Present"
+            Name            = "Contoso"
+            State           = "Started"
+            PhysicalPath    = "c:\inetpub\Contoso"
+            DefaultPage     = "Default.aspx"
+            BindingInfo     = MSFT_xWebBindingInformation 
+                             { 
+                               Protocol  = "HTTP" 
+                               Port      = 80
+                             } 
+        }
+
+        xWebAppPool ContosoAppPool
+        {
+            Ensure    = "Present"
+            Name      = "ContosoAppPool"
+            State     = "Started"
+        }
+
+        xWebApplication ContosoWebApp
+        {
+            Ensure       = "Present"
+            Name         = "WebApp"
+            WebAppPool   = "ContosoAppPool"
+            Website      = "Contoso"
+            PhysicalPath = "C:\Program Files\Contoso\Corp WebApp"
+        }
+ 
+        xWebVirtualDirectory Images
+        {
+            Ensure         = "Present"
+            Website        = "Contoso"
+            WebApplication = "WebApp"
+            Name           = "Images"
+            PhysicalPath   = "C:\Program Files\Contoso\Common Images"
+        }
+
+        xWebConfigKeyValue ModifyWebConfig 
+        { 
+          Ensure         = "Present" 
+          ConfigSection  = "AppSettings" 
+          key            ="emailto"
+          value          ="me@nowhere.net" 
+          IsAttribute    = $false 
+          WebsitePath    = "IIS:\Sites\Contoso\WebApp\"   
+        } 
+    }
+}
+
+Web -OutputPath c:\temp\Web 
+
+#Start-DscConfiguration -Path c:\temp\Web\ -Wait -Verbose -Force
